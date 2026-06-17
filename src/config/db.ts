ad4fae97d0cffc7f9d770f5d15b85e71e637c60c
neegarw <nigarmurvtzad@gmail.com>
2026-06-17 15:52:44 +0400

    fix railway database config

--- src/config/db.ts
+++ src/config/db.ts
@@ -12,9 +12,10 @@ dotenv.config();
 export const sequelize = new Sequelize(
   process.env.DB_NAME as string,
   process.env.DB_USER as string,
-  '',
+  process.env.DB_PASSWORD as string,
   {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
     logging: false,
   }
