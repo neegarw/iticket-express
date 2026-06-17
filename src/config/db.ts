c2d3932876b5c8b2f7bb6419b24d762b43a207cd
neegarw <nigarmurvtzad@gmail.com>
2026-06-17 16:02:35 +0400

    fix server port type

--- src/config/db.ts
+++ src/config/db.ts
@@ -15,7 +15,7 @@ export const sequelize = new Sequelize(
   process.env.DB_PASSWORD as string,
   {
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    port: Number(process.env.DB_PORT || 3306),
     dialect: "mysql",
     logging: false,
   }
